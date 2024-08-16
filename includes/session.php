c2007527035568fc7acca3ae4b74ea296db755a1
baquy2293 <op347766gmail.com>
2024-08-16 16:50:19 +0700

    create(function,database sesion):  Create some commonly used functions,database

--- includes/session.php
+++ includes/session.php
@@ -1,11 +1,57 @@
 <?php
-//Start session
-session_start();
-//Check whether the session variable SESS_MEMBER_ID is present or not
-if (!isset($_SESSION['user_id']) || (trim($_SESSION['user_id']) == '')) {
-    header("location: index.php");
-    exit();
-}
-$session_id=$_SESSION['user_id'];
+if (!defined('_INCODE')) die('Access Deined...');
+/*Chứa các hàm liên quan đến thao tác session*/
 
-?>
+//Hàm gán session
+function setSession($key, $value){
+    if (!empty(session_id())){
+        $_SESSION[$key] = $value;
+        return true;
+    }
+
+    return false;
+}
+
+//Hàm đọc session
+function getSession($key=''){
+    if (empty($key)){
+        return $_SESSION;
+    }else{
+        if (isset($_SESSION[$key])){
+            return $_SESSION[$key];
+        }
+    }
+
+    return false;
+}
+
+//Hàm xoá session
+function removeSession($key=''){
+    if (empty($key)){
+        session_destroy();
+        return true;
+    }else{
+        if (isset($_SESSION[$key])){
+            unset($_SESSION[$key]);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//Hàm gán flash data
+function setFlashData($key, $value){
+    $key = 'flash_'.$key;
+    return setSession($key, $value);
+}
+
+//Hàm đọc flash data
+function getFlashData($key){
+    $key = 'flash_'.$key;
+    $data = getSession($key);
+
+    removeSession($key);
+
+    return $data;
+}
